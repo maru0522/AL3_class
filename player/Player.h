fbb8d4696d4a7c79ae4d5e4539f3e22b83eadee9
maru0522 <k021g1258@g.neec.ac.jp>
2022-07-27 12:07:25 +0900

    前回時の記録。覚えてない

---
+++ player/Player.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+#include <cassert>
+#include "input.h"
+#include "DebugText.h"
+#include "PlayerBullet.h"
+#include <memory>
+#include <list>
+
+class Player
+{
+public:
+	// 初期化
+	void Initialize(Model* model,uint32_t textureHandle);
+	// 移動
+	void Move();
+	// 軸回転
+	void Rotate();
+	// 攻撃
+	void Attack();
+	// 更新
+	void Update();
+	// 描画
+	void Draw(ViewProjection viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	Input* input_ = nullptr;
+	DebugText* debugText_ = nullptr;
+
+	// 弾
+	std::list<std::unique_ptr<PlayerBullet>> bullets_;
+};
